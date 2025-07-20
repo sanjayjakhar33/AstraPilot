ca568457f22f14c3ee8c97c67b863e969ab3d349
sanjayjakhar33 <sanjayjakhar33@gmail.com>
2025-07-20 20:42:36 +0530

    Create __init__.py

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,5 @@
+from .user import User
+from .payment import Payment
+from .license import License
+from .seodata import SeoData        # Only if you created seodata.py
+from .social import Social         # Only if you created social.py
