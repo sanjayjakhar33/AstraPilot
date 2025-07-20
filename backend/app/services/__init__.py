1bda05a8351fd97b02cdd8b7bb71639c323b75c7
sanjayjakhar33 <sanjayjakhar33@gmail.com>
2025-07-20 20:54:13 +0530

    Create __init__.py

---
+++ backend/app/services/__init__.py
@@ -0,0 +1,6 @@
+from .auth_service import *
+from .payment_service import *
+from .license_service import *
+from .social_service import *
+from .seo_service import *
+from .dashboard_service import *
